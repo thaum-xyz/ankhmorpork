5eaf1327eba438fff8e6d7a3919bd87e4f2e634a
paulfantom <pawel@krupa.net.pl>
2020-06-14 11:13:14 +0200

    deploy: use pushgateway to export deployment metrics

--- deploy.sh
+++ deploy.sh
@@ -1,51 +1,44 @@
 #!/bin/bash
 
-METRICS_FILE="/var/lib/node_exporter/deployment.prom"
 REPO_DIR=$(pwd)
-RUN_AS="root"
 
 metrics() {
-    echo "INFO: Updating metrics"
-    local end success error
-    if [ ! -f "$METRICS_FILE" ]; then
-        success=0
-        error=0
-    else
-        error=$(grep "error" "${METRICS_FILE}" | cut -d' ' -f2)
-        success=$(grep "success" "${METRICS_FILE}" | cut -d' ' -f2)
-    fi
-    if [ "$UPDATE_STATE" -eq "0" ]; then
-        error=0
-        success=$(( success + 1))
-    else
-        success=0
-        error=$(( error + 1))
-    fi
-    end="$(date +%s)"
-    METRICS=$(mktemp)
-    cat <<EOF > "$METRICS"
+	if [ "$UPDATE_STATE" -eq "0" ]; then
+		error=0
+		success=1
+	else
+		success=0
+		error=1
+	fi
+	end="$(date +%s)"
+        if [ -z "$PUSHGATEWAY_URL" ]; then
+                echo "INFO: PUSHGATEWAY_URL not defined, metrics won't be sent"
+        else
+                cat <<EOF | curl --data-binary @- "${PUSHGATEWAY_URL}/metrics/job/deploy/instance/$(hostname)"
 # HELP deployment_duration_seconds Time spent on deploying stack
 # TYPE deployment_duration_seconds gauge
 deployment_duration_seconds $((end - START))
-# HELP deployment_total Total number of deployments
-# TYPE deployment_total counter
-deployment_total{status="error"} $error
-deployment_total{status="success"} $success
+# HELP deployment_status_success Return 1 if deployment was successful
+# TYPE deployment_status_success gauge
+deployment_status_success $success
+# HELP deployment_status_failure Return 1 if deployment failed
+# TYPE deployment_status_failure gauge
+deployment_status_failure $success
 EOF
-    chmod a+r "$METRICS"
-    mv "$METRICS" "$METRICS_FILE"
+                echo "$(date +"%F %T") INFO: Statistics exported. All done."
+        fi
 }
 
 update_repo() {
 	echo "INFO: Updating code repository"
 	# Clean repository and revert all local changes as well as 10 prev commits
-	su "$RUN_AS" -c "git clean -xfd"
-	su "$RUN_AS" -c "git reset --hard HEAD"
+	git clean -xfd
+	git reset --hard HEAD
 
 	## Synchronize repository
 	prev=$(git rev-parse HEAD)
-	su "$RUN_AS" -c "git reset --hard HEAD~10"
-	su "$RUN_AS" -c "git pull"
+	git reset --hard HEAD~10
+	git pull
 
 	if git diff --name-only --diff-filter=AMDR --cached "${prev}" | grep -q "ansible"; then
 		return 1
@@ -69,26 +62,21 @@ update_hosts() {
 	fi
 
 	cd "${REPO_DIR}/ansible"
-	su "$RUN_AS" ansible-galaxy install $params -r roles/requirements.yml
-	su "$RUN_AS" ansible-playbook 00_site.yml 3>&1 || exit 1
+	ansible-galaxy install $params -r roles/requirements.yml
+	ansible-playbook 00_site.yml 3>&1 || exit 1
 }
 
 START="$(date +%s)"
 echo "Start update at $(date)"
 UPDATE_STATE=1 # 0 - update done, 1 - update failed
 ARA_SERVER="$1"
+PUSHGATEWAY_URL="$2"
 
 trap metrics EXIT
 set -euo pipefail
 
-if [ "$EUID" -ne 0 ]; then
-	echo "ERR: Please run as root"
-	exit 1
-fi
-
 cd "$(dirname "${BASH_SOURCE[0]}")"
 REPO_DIR="$(pwd)"
-RUN_AS="$(stat --format '%U' .git)"
 
 if update_repo; then
 	UPDATE_STATE=0
