a540ed6de355f230ca18ee95061dadcf389bca44
paulfantom <pawel@krupa.net.pl>
2020-06-21 12:52:07 +0200

    hack: add checking for leaked secrets

---
+++ hack/checksecrets.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Go to top-level
+cd "$(git rev-parse --show-toplevel)"
+
+LEAKS=""
+
+for file in $(find apps/ base/ -name *.yaml -exec grep -E 'kind:[[:space:]]*Secret' -l {} \;); do
+	new=$(gojsontoyaml -yamltojson < $file | jq -cr '..| .data?, .stringData? | select(type != "null")')
+	if [ "$new" != "" ]; then
+		LEAKS="${file}\n${LEAKS}"
+	fi
+done
+
+if [ "$LEAKS" != "" ]; then
+	echo -e "Files with secure data leak:"
+	echo -e "${LEAKS}"
+	exit 1
+fi
+
+exit 0
