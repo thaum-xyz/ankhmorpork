1ce2e7e9eaf6070f30637ce5a3f79987e6fcdafc
Paweł Krupa <pawel@krupa.net.pl>
2020-06-26 14:56:58 +0200

    hack: Fix parallelism in checkimage script

--- hack/checkimages.sh
+++ hack/checkimages.sh
@@ -22,30 +22,30 @@ EOM
 )
 
 check_cross_compatibility() {
-	local image="${1}"
-	local manifest="${2}"
-	local err=0
-	local arch_list
+        local image="${1}"
+        local manifest="${2}"
+        local err=0
+        local arch_list
 
-	for exclude in ${MULTI_ARCH_EXCLUDED}; do
-		if [[ "${image}" =~ ${exclude} ]]; then
-			echo "WARN: Skipping validating cross-arch compatibility for ${image}"
-			return
-		fi
-	done
-	
-	arch_list="$(echo "${manifest}" | jq -cr '..| .architecture?, .Architecture? | select(type != "null") | select(. != "" )'  | sort | uniq)"
-	for arch in ${CPU_ARCHS}; do
-		if ! grep -q "${arch}$" <<< "$arch_list"; then	
-			echo "ERR : Image ${image} does not support ${arch} !"
-			err=1
-		fi
-	done
-	if [ "$err" -eq 1 ]; then
-		exit 129
-	else
-		echo "INFO: Image ${image} is compatible with specified CPU architectures"
-	fi
+        for exclude in ${MULTI_ARCH_EXCLUDED}; do
+                if [[ "${image}" =~ ${exclude} ]]; then
+                        echo "WARN: Skipping validating cross-arch compatibility for ${image}"
+                        return
+                fi
+        done
+
+        arch_list="$(echo "${manifest}" | jq -cr '..| .architecture?, .Architecture? | select(type != "null") | select(. != "" )'  | sort | uniq)"
+        for arch in ${CPU_ARCHS}; do
+                if ! grep -q "${arch}$" <<< "$arch_list"; then
+                        echo "ERR : Image ${image} does not support ${arch} !"
+                        err=1
+                fi
+        done
+        if [ "$err" -ne 0 ]; then
+                exit 129
+        else
+                echo "INFO: Image ${image} is compatible with specified CPU architectures"
+        fi
 }
 
 
@@ -56,15 +56,28 @@ cd "$(git rev-parse --show-toplevel)"
 IMAGES=""
 
 for file in $(find apps/ base/ -name *.yaml -exec grep "image" -l {} \;); do
-	new=$(gojsontoyaml -yamltojson < "$file" | jq -cr '..| .image? | select(type != "null")')
-	IMAGES="${new} ${IMAGES}"
+        new=$(gojsontoyaml -yamltojson < "$file" | jq -cr '..| .image? | select(type != "null")')
+        IMAGES="${new} ${IMAGES}"
 done
 
+pids=()
 for image in $(echo -e "${IMAGES}" | tr ' ' '\n' | sort -f | uniq); do
-	(
-		echo "INFO: Inspecting ${image} ..."
-		info=$(manifest-tool inspect --raw "${image}")
-		check_cross_compatibility "${image}" "${info}"
-	) &
+        (
+                echo "INFO: Inspecting ${image} ..."
+                info=$(manifest-tool inspect --raw "${image}")
+                check_cross_compatibility "${image}" "${info}"
+        ) &
+        pids+=("$!")
 done
-wait
+
+EXIT_CODE=0
+for job in "${pids[@]}"; do
+        CODE=0;
+        wait ${job} || CODE=$?
+        if [[ "${CODE}" != "0" ]]; then
+                echo "At least one image is not compatible with specified CPU architectures" ;
+                EXIT_CODE=$CODE;
+        fi
+done
+
+exit $EXIT_CODE
