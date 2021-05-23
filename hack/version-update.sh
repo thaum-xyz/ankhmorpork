e33960f295c7f3cd017024afd89cef6477a65862
paulfantom <pawel@krupa.net.pl>
2021-05-23 18:51:12 +0200

    *: autoupdater

---
+++ hack/version-update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Test only for homer for now
+FILE=apps/homer/jsonnet/settings.yaml
+
+get_latest_version() {
+  echo >&2 "Checking release version for ${1}"
+  curl --retry 5 --silent --fail -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/${1}/releases/latest" | jq '.tag_name' | tr -d '"v'
+}
+
+REPO=$(grep version-updater-repo "$FILE" | rev | cut -d: -f1 | xargs | rev)
+
+LATEST="$(get_latest_version "$REPO")"
+CURRENT=$(grep 'version:' "$FILE" | cut -d\" -f2)
+
+echo "Current: $CURRENT, Latest: $LATEST"
+if [ "$CURRENT" == "$LATEST" ]; then
+    echo "Nothing to do"
+    exit 0
+fi
+sed -i "s/$CURRENT/$LATEST/g" "$FILE"
