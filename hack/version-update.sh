41b78ab759c8f58fdcedc5f527122291d7d5ca5b
paulfantom <pawel@krupa.net.pl>
2021-05-23 19:33:36 +0200

    *: fixes to version updater

--- hack/version-update.sh
+++ hack/version-update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euo pipefail
+set -eo pipefail
 
 # Test only for homer for now
 FILE=apps/homer/jsonnet/settings.yaml
@@ -14,6 +14,11 @@ REPO=$(grep version-updater-repo "$FILE" | rev | cut -d: -f1 | xargs | rev)
 
 LATEST="$(get_latest_version "$REPO")"
 CURRENT=$(grep 'version:' "$FILE" | cut -d\" -f2)
+
+if [ "$LATEST" == "" ]; then
+    echo "Latest version detection failed"
+    exit 1
+fi
 
 echo "Current: $CURRENT, Latest: $LATEST"
 if [ "$CURRENT" == "$LATEST" ]; then
