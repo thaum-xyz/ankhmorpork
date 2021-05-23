2b66b820fcaf4445104fd58cf46445a9366d9b1c
paulfantom <pawel@krupa.net.pl>
2021-05-23 21:04:46 +0200

    *: run updater on multiple repositories
    Signed-off-by: paulfantom <pawel@krupa.net.pl>

--- hack/version-update.sh
+++ hack/version-update.sh
@@ -2,27 +2,37 @@
 
 set -eo pipefail
 
-# Test only for homer for now
-FILE=apps/homer/jsonnet/settings.yaml
-
 get_latest_version() {
-  echo >&2 "Checking release version for ${1}"
-  curl --retry 5 --silent --fail -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/${1}/releases/latest" | jq '.tag_name' | tr -d '"v'
+    echo >&2 "Checking release version for ${1}"
+    curl --retry 5 --silent --fail -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/${1}/releases/latest" | jq '.tag_name' | tr -d '"v'
 }
 
-REPO=$(grep version-updater-repo "$FILE" | rev | cut -d: -f1 | xargs | rev)
+DIRECTORY="${1}"
 
-LATEST="$(get_latest_version "$REPO")"
-CURRENT=$(grep 'version:' "$FILE" | cut -d\" -f2)
+# Scan for files with `version-updater-repo` comment
+FILES=$(grep --exclude=*./vendor/ -rl "$DIRECTORY" -e "github-repository:")
 
-if [ "$LATEST" == "" ]; then
-    echo "Latest version detection failed"
-    exit 1
-fi
+# Iterate over each file searching for mark
+for f in $FILES; do
+    REPOS=$(grep github-repository "$f" | rev | cut -d: -f1 | xargs | rev)
+    # Deduplicate
+    REPOS=$(echo "${REPOS[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')
 
-echo "Current: $CURRENT, Latest: $LATEST"
-if [ "$CURRENT" == "$LATEST" ]; then
-    echo "Nothing to do"
-    exit 0
-fi
-sed -i "s/$CURRENT/$LATEST/g" "$FILE"
+    for r in $REPOS; do
+        LATEST="$(get_latest_version "$r")"
+        CURRENT=$(grep 'version:' "$f" | cut -d: -f2 | cut -d# -f1 | xargs)
+
+        if [ "$LATEST" == "" ]; then
+            echo "Latest version detection failed"
+            exit 1
+        fi
+
+        echo >&2 "Current: $CURRENT, Latest: $LATEST"
+        if [ "$CURRENT" == "$LATEST" ]; then
+            echo >&2 "Nothing to do for $r"
+            continue
+        fi
+        sed -i "s/$CURRENT/$LATEST/g" "$f"
+    done
+done
+
