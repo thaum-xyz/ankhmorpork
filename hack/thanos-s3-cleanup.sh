a4646ca9faccacca16ab27b949c531017511a801
paulfantom <pawel@krupa.net.pl>
2023-10-21 16:28:46 +0200

    hack: add script to cleanup thanos S3 bucket

---
+++ hack/thanos-s3-cleanup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Script used to clean up S3 bucket used by thanos. It iterates over folders in minio S3 bucket and checks if there
+# is a file called `meta.json` or `deletion-mark.json`. Based on those files which it performs the following actions:
+# - If it finds `deletion-mark.json` it deletes the folder and all folder versions from S3 bucket. This is because
+#   thanos compactor already marked this folder as ready for deletion and compactor can leave the folder after is run.
+# - If it doesn't find `meta.json` it also removes the folder and all folder versions from S3 bucket. This is because
+#   without `meta.json` the folder is not used by thanos and it can be safely removed.
+
+MINIO_URL="http://localhost:9000"
+
+# Log in to minio
+mc alias set minio $MINIO_URL $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
+
+# Get the list of folders
+folders=$(mc ls minio/$MINIO_BUCKET | awk '{print $5}')
+
+# Loop through the folders
+for folder in $folders; do
+    # Check if there is a file called `deletion-mark.json` in the folder
+    deletion_mark=$(mc ls minio/$MINIO_BUCKET/$folder | grep "deletion-mark.json")
+    # Check if there is a file called `meta.json` in the folder
+    meta=$(mc ls minio/$MINIO_BUCKET/$folder | grep "meta.json")
+
+    echo "Checking $folder... "
+
+    # If there is a file called `deletion-mark.json` in the folder
+    if [ -n "$deletion_mark" ]; then
+        # Remove the folder and all folder versions from S3 bucket
+        mc rm --recursive --force --versions minio/$MINIO_BUCKET/$folder
+    # If there is no file called `meta.json` in the folder
+    elif [ -z "$meta" ]; then
+        # Remove the folder and all folder versions from S3 bucket
+        mc rm --recursive --force --versions minio/$MINIO_BUCKET/$folder
+    fi
+done
